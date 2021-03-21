cd01b9e9e843cb6a412f9bf9cef5a7094aa28f42
Milan5840 <60858360+Milan5840@users.noreply.github.com>
2021-03-21 01:20:24 -0500

    Register

---
+++ screens/account/Login.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { StyleSheet, ScrollView, Text, View, Image } from 'react-native'
+import { Divider } from 'react-native-elements'
+
+export default function Login() {
+    return (
+        <ScrollView>
+      <Image
+        source={require("../../assets/restaurant-logo.png")}
+        resizeMode="contain"
+        style={styles.image}
+      />
+      <View style={styles.container}>
+          <Text>Login Form</Text>
+          <createAccount/>
+      </View>
+      <Divider style={styles.divider}/>
+        </ScrollView>      
+    )
+}
+
+function createAccount(props){
+    return (
+        <Text 
+        style={styles.register}
+        onPress={() => console.log("")}
+        >
+        ¿Aun no tienes una cuenta?{" "}
+        <Text style={styles.btnRegister}>
+            Registrate
+        </Text>
+        </Text>
+    )
+}
+
+const styles = StyleSheet.create({
+    image: {
+        height: 150,
+        width: "100%",
+        marginBottom: 20
+    }, 
+    container: {
+        marginHorizontal: 40
+    },
+    divider: {
+        backgroundColor: "#442484",
+        margin: 40
+    },
+    register: {
+        marginTop: 15,
+        marginHorizontal: 10,
+        alignSelf:"center"
+    },
+    btnRegister: {
+      color: "#442484",
+      fontWeight: "bold"
+    }
+})
+
